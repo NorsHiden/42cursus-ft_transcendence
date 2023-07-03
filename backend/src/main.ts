2031e53af70cd32667dcf1ac35485d04a5e85a4e
HxX2 <zaka.hanix@gmail.com>
2023-07-04 00:03:06 +0100

    cleanup and added some usefulness

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as morgran from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const { PORT } = process.env;
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('api');
+	app.useGlobalPipes(new ValidationPipe());
+	app.use(morgran('dev'));
+
+	try {
+		await app.listen(PORT, () => {
+			console.log('Server is running on port ' + PORT )
+		});
+	} catch (error) {
+		console.log('Error starting server', error);
+	}
+
 }
 bootstrap();
---
+++ backend/src/utils/consts.ts
@@ -0,0 +1,10 @@
+export enum Routes {
+	CHAT = 'chat',
+	AUTH = 'auth',
+	USERS = 'users',
+}
+
+export enum Services {
+	Auth = 'AUTH_SERVICE',
+	Users = 'USERS_SERVICE',
+}
