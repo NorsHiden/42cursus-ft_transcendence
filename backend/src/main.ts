9b49fd1f07896b7e022a71b1dddf65c8459f1825
Nasroallah Elidrissi <nelidris@e1r12p2.1337.ma>
2023-07-15 17:18:31 +0100

    achievements

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,17 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import * as morgran from 'morgan';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
-
+  
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  app.use(morgran('dev'));
-
+  app.use(morgan('dev'));
   try {
     await app.listen(PORT, () => {
       console.log('Server is running on port ' + PORT);
