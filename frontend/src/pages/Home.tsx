62397ccd313aabf418ae1cea6fa598bc16bc708d
aamoussa <amoussaouianas@gmail.com>
2023-08-07 01:50:21 +0100

    Two tasks finished  Style the Navbar and Style Game modes
     On branch 23-design-home-page-components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,19 @@
 // import { Outlet } from "react-router-dom";
+import SideBar from "../components/SideBar";
+import NavBar from "../components/home/NavBar";
+import FriendsBar from "../components/home/FriendsBar";
+import ButtonAct from "../components/home/ButtonAct";
+import GameModes from "../components/home/GamModes";
 
 function Home() {
 
     return (
         <div className="home">
-            <h1>Welcome to home</h1>
+            <NavBar/>
+            {/* <SideBar/> */}
+            <GameModes/>
+            {/* <ButtonAct/> */}
+            {/* <FriendsBar/> */}
         </div>
     )
 }
