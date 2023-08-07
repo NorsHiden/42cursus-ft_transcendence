5df7f3244deb2a013b7e770da2d67a821ec1343b
aamoussa <amoussaouianas@gmail.com>
2023-08-07 02:17:23 +0100

    Style Game modes task done
    Reusable Shape Component with Dynamic Content
    
    This React component is designed to create and display a graphical shape (likely a polygon) with customizable attributes and dynamic content. It offers the flexibility to design unique shapes and easily integrate them into your application.
    
    checkout the interfce to understand how to use it.
    
    Props:
    
        childComp (optional): This prop allows you to pass any React component as the content of the shape. This component will be rendered inside the shape.
    
        stroke (optional): Specifies the stroke color of the shape's outline. The color should be provided in the format "[color:#4E301F]" using a hexadecimal value.
    
        fill (optional): Sets the fill color of the shape's interior. The color format is the same as for the stroke prop.
    
        margine (optional): Defines the margins for the shape using Tailwind CSS margin classes.
    
        polygonpoints (optional): Specifies the points that define the shape's outline. This prop uses the CSS clip-path property's polygon syntax. You can use the provided string to create custom shapes or refer to external resources for shape generation.
    
        width (optional): Determines the width of the shape. It should be a numeric value in pixels (px).
    
        height (optional): Similar to width, this prop sets the height of the shape in pixels.
    
        strokesize (optional): Specifies the size of the stroke for the shape's outline. It should be a numeric value in pixels (px).
    
    On branch 23-design-home-page-components

---
+++ frontend/src/components/CornerLinedCard.tsx
@@ -0,0 +1,61 @@
+import {twMerge} from "tailwind-merge";
+import React from 'react'
+
+/** this is a reusable component. sift message la biti t3rf kifach tst3mlo hehe (gha 9ra l interface a yban lik lblan)*/
+
+
+interface ParentCompProps {
+    childComp?: React.ReactNode;// this the card content pass any component you want
+    stroke?: string; //stroke color the string should be in this format "[color:#4E301F]"  use hexa in value
+    fill?: string;   // fill color same format as stroke
+    margine?: string;  // same forma as tailwind margines
+    polygonpoints?:string; // this is the points that make ur shape searche about polygone clip-path to understand more or visite this website to make ur own shapes (https://www.cssportal.com/css-clip-path-generator/) example of the string input "14.42% -0.15%, 100% 0%, 100.23% 80.16%, 87.12% 100.88%, 0% 100%, -0.5% 20.08%"
+    width?:number; // this is the width of the shape it should be number you may passe it ass porp like this width={1} the unit is always px
+    height?:number; // same as width
+    strokesize?:number // this the size of stroke you can pass props same as widht it should be number as well
+}
+
+function CornerLinedCard(props:ParentCompProps){
+    const {childComp, stroke, fill, margine, polygonpoints} = props;
+    const width:string = "w-[" + props.width + "px]"
+    const height:string = "before:[padding-top:"+props.height+"px;]"
+    const strokesizewidth:string = "w-[" + (props.width - props.strokesize) + "px]"
+    const strokesizeheight:string = "before:[padding-top:"+(props.height - props.strokesize)+"px;]" 
+    // const strokesizez:string = 
+    return (
+        <div className="cursor-pointer">
+            <div style={{'--polygon-points':polygonpoints}} className={
+                twMerge("modestroke inline-block [filter: url(#round)] ",
+                width,
+                stroke,
+                    "before:content-[''] before:block ",
+                    height,
+                    props.margine,
+                )
+            } >
+
+                
+                <div className={
+                    twMerge("modefill center flex items-center justify-center",
+                    strokesizewidth,
+                    strokesizeheight,  
+                    fill
+                    )
+                } >
+                    {childComp}
+                </div>
+
+                <svg style={{ visibility: "hidden", position: "absolute" }} width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                        <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+                        </filter>
+                    </defs>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default CornerLinedCard;
