72fa9d9b995e921023eb233569e29eece2b2f675
Ereur <amoussaouianas@gmail.com>
2023-08-24 12:50:09 +0100

    login component

--- frontend/src/components/login/LoginView.tsx
+++ frontend/src/components/login/LoginView.tsx
@@ -2,9 +2,13 @@ import google from "/google.svg"
 import ft from "/42.svg"
 import discord from "/discord.svg"
 import logo from "/logo.svg"
+import LoginViewController from "./LoginViewController"
 
 const LoginView = () => {
-  return (
+    const {googleLink,ftLink,discordLink} = LoginViewController()
+    
+    // console.log(googleLink,ftLink,discordLink)
+    return (
     <div className="grid grid-cols-2 h-[100vh] w-[100wh]">
         <div id="login_section" className="w-full h-full">
             <div id="logo" className="flex justify-center mt-[7.59vh]">
@@ -14,9 +18,15 @@ const LoginView = () => {
                 <h1 className="font-sans font-extrabold text-[2.29vw] ">Are you ready to play!</h1>
                 <h2 className="text-[1.66vw] font-sans font-extrabold text-[#5E6069] mt-[1.57vh]" >login</h2>
                 <div id="providers" className="flex mt-[3.51vh]">
-                    <img src={google} alt="google" className="w-[5vw] w-[5vw] mr-[2.18vw]"/>
-                    <img src={discord} alt="discord" className="w-[5vw] w-[5vw] mr-[2.18vw]" />
-                    <img src={ft} alt="fortytwo" className="w-[5vw] w-[5vw]"/>
+                    <a href="/">
+                        <img src={google} alt="google" className="w-[5vw] w-[5vw] mr-[2.18vw]" />
+                    </a>
+                    <a href="/">
+                        <img src={discord} alt="discord" className="w-[5vw] w-[5vw] mr-[2.18vw]" />
+                    </a>
+                    <a href="/">
+                        <img src={ft} alt="fortytwo" className="w-[5vw] w-[5vw]"/>
+                    </a>
                 </div>
             </div>
         </div>
