c14d07c06d4beb12f1ff39ac5087cfbc729aec19
Ereur <amoussaouianas@gmail.com>
2023-08-24 12:46:01 +0100

    getting url data passed to the componnent as queries ?redirect=blabla

--- frontend/src/components/login/LoginModal.tsx
+++ frontend/src/components/login/LoginModal.tsx
@@ -1,1 +1,10 @@
+import { useSearchParams } from "react-router-dom"
 
+const LoginModal = ()=>{
+    const [searchParams] = useSearchParams();
+    const redirect = searchParams.get('redirect')
+    
+    return {redirect}
+}
+
+export default LoginModal
