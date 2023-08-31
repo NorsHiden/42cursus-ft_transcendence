479db139b2d51587308ef0e8aa22f54ae342c9ac
Ereur <amoussaouianas@gmail.com>
2023-08-31 15:21:39 +0100

    adding loginview

--- frontend/src/components/login/LoginView.tsx
+++ frontend/src/components/login/LoginView.tsx
@@ -6,7 +6,8 @@ import LoginViewController from "./LoginViewController"
 
 const LoginView = () => {
     const {googleLink,ftLink,discordLink} = LoginViewController()
-    
+    // const test = "<script>function hello(){console.log(\"hello\")}</script>"
+
     // console.log(googleLink,ftLink,discordLink)
     return (
     <div className="grid grid-cols-2 h-[100vh] w-[100wh]">
@@ -16,16 +17,16 @@ const LoginView = () => {
             </div>
             <div id="providers_section" className="flex flex-col mt-[18.14vh] items-center w-full h-full">
                 <h1 className="font-sans font-extrabold text-[2.29vw] ">Are you ready to play!</h1>
-                <h2 className="text-[1.66vw] font-sans font-extrabold text-[#5E6069] mt-[1.57vh]" >login</h2>
+                <h2 className="text-[1.66vw] font-sans font-extrabold text-[#5E6069] mt-[1.57vh]" >Login</h2>
                 <div id="providers" className="flex mt-[3.51vh]">
                     <a href="">
-                        <img src={google} alt="google" className="w-[5vw] w-[5vw] mr-[2.18vw]" />
+                        <img src={google} alt="google" className="w-[5vw] w-[5vw] mr-[2.18vw] hover:brightness-90 " />
                     </a>
                     <a href="/">
-                        <img src={discord} alt="discord" className="w-[5vw] w-[5vw] mr-[2.18vw]" />
+                        <img src={discord} alt="discord" className="w-[5vw] w-[5vw] mr-[2.18vw] hover:brightness-110" />
                     </a>
                     <a href="/api/auth/42/login">
-                        <img src={ft} alt="fortytwo" className="w-[5vw] w-[5vw]"/>
+                        <img src={ft} alt="fortytwo" className="w-[5vw] w-[5vw] hover:brightness-90"/>
                     </a>
                 </div>
             </div>
