318c9895e835d42433459144b9ecf9402a6bc9d4
Ereur <amoussaouianas@gmail.com>
2023-11-16 16:42:07 +0100

    set the polygone as a background

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,21 +1,38 @@
-import React, { useRef } from 'react';
-import Polygon from './Polygon';
 
-type CardProps = {
+import React, { useRef,useEffect, useState} from 'react';
+import Polygon, {PolygonProps} from './Polygon';
+
+// Omit<T, K> creates a new type by picking all properties from T and removing K.
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+type CardProps = Omit<PolygonProps, 'width' | 'height'> & {
   className?: string;
   children?: React.ReactNode;
 };
 
-const Card: React.FC<CardProps> = ({ className, children }) => {
+const Card: React.FC<CardProps> = ({ className, children, ...PolygonProps}) => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const cardRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    if (cardRef.current) {
+      setDimensions({
+        width: cardRef.current.offsetWidth,
+        height: cardRef.current.offsetHeight,
+      });
+    }
+  }, []);
+
   return (
-    <div ref={cardRef} className={className}>
-      {children}
+    <div ref={cardRef} className={`relative ${className}`}>
       <Polygon
-        width={cardRef.current?.offsetWidth}
-        height={cardRef.current?.offsetHeight}
+        {...PolygonProps}
+        width={dimensions.width}
+        height={dimensions.height}
       />
+      <div className="relative z-10">
+        {children}
+      </div>
     </div>
   );
 };
