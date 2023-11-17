0b0bda5722ef2c5e0c34e4de8a655aa9f5e876c2
Ereur <amoussaouianas@gmail.com>
2023-11-17 20:55:25 +0100

    adding icons component

---
+++ frontend/src/components/Icons/icontype.tsx
@@ -0,0 +1,11 @@
+
+export type IconProps = {
+    fill?: string;
+    color?: string;
+    size?: string | number;
+    width?: string | number;
+    height?: string | number;
+    strokeWidth?: string | number;
+    stroke?: string;
+    className?: string;
+};
