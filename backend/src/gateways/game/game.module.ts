2a7d67f98dc501294273d058987a5a600a7fee43
Peannut <zakariaspam0@gmail.com>
2023-11-17 10:45:49 +0100

    jwtService added

--- backend/src/gateways/game/game.module.ts
+++ backend/src/gateways/game/game.module.ts
@@ -3,6 +3,7 @@ import { Services } from 'src/utils/consts';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { JwtService } from '@nestjs/jwt';
 import { GameService } from './services/game.service';
 import { GameGateway } from './gateway/game.gateway';
@@ -33,6 +34,9 @@ import { GameGateway } from './gateway/game.gateway';
   imports: [],
 >>>>>>> ccf63eb (game init)
 =======
+=======
+import { JwtService } from '@nestjs/jwt';
+>>>>>>> eaa8a70 (jwtService added)
 import { GameService } from './services/game.service';
 import { GameGateway } from './gateway/game.gateway';
 
@@ -44,6 +48,7 @@ import { GameGateway } from './gateway/game.gateway';
       provide: Services.Game,
       useClass: GameService,
     },
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -62,6 +67,9 @@ import { GameGateway } from './gateway/game.gateway';
 >>>>>>> 26804f8 (jwtService added)
 =======
 >>>>>>> 475422b (game init)
+=======
+    JwtService,
+>>>>>>> eaa8a70 (jwtService added)
     GameGateway,
   ],
   exports: [
