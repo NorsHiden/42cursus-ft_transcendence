1faa9787fc7966d8505e6e32ab44022cbafc0573
Peannut <zakariaspam0@gmail.com>
2023-11-17 15:25:35 +0100

    joining rooms

--- backend/src/gateways/game/gateway/game.gateway.ts
+++ backend/src/gateways/game/gateway/game.gateway.ts
@@ -1,9 +1,14 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { Inject, UseGuards } from '@nestjs/common';
 import { Namespaces, Services } from 'src/utils/consts';
 import { IGameService } from '../interfaces/game.interface';
 import { WsGuard } from 'src/gateways/guards/ws.guard';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { WebSocketServer } from '@nestjs/websockets';
 
 @WebSocketGateway({
@@ -22,7 +27,14 @@ export class GameGateway {
   ) {}
 
   @SubscribeMessage('test')
-  test() {
-    console.log('test');
+  async test(
+    @ConnectedSocket() client: Socket,
+    @MessageBody('name') name: string,
+  ) {
+    await this.gameService.handleConnection(client, client.id);
+    console.log(this.gameService.getId(client.id));
+    client.join('some_room');
+    this.server.to('some_room').emit('test2', 'test2');
+    return name;
   }
 }
