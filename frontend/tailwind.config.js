16059cb8202da517e8624175ba6a75ecab803da8
Ereur <amoussaouianas@gmail.com>
2023-11-17 20:54:02 +0100

    Alias Config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,19 +2,38 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       gridTemplateColumns: {
         layout: '8% repeat(4, 1fr) 8%',
       },
+      colors: {
+        vibrantOrange: '#FE5821',
+        background:'#1B191D',
+        'input-color': '#1E1F23',
+        'input-border-color':'#3E4048',
+        'input-text-color':'#71717A'
+      },
+      spacing: {
+        '17': '4.25rem',
+      }
     },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
       serif: ['Rowdies', 'serif'],
       mono: ['ui-monospace', 'monospace'],
+      'inter': ['Inter', 'sans-serif'],
     },
     fontSize: {
       sm: '0.8rem',
       base: '1rem',
+      lg: '1.25rem',
       xl: '1.30rem',
       '2xl': '1.563rem',
       '3xl': '1.953rem',
