f1d419656f138c3d2e47df15b1fed4d54bb51db8
Ereur <amoussaouianas@gmail.com>
2023-11-17 20:41:10 +0100

    adding a toast in case of input errors at the buttom left

--- frontend/src/components/PostLogin/PostLoginView.tsx
+++ frontend/src/components/PostLogin/PostLoginView.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect} from 'react';
 import logo from '/logo.svg';
-import aamoussa from '/aamoussa.jpeg';
-import update from '/update.svg';
-import CornerLinedCardTest from '../CornerLinedCard/CornerLinedCardTest';
+// import aamoussa from '/aamoussa.jpeg';
+// import update from '/update.svg';
+// import CornerLinedCardTest from '../CornerLinedCard/CornerLinedCardTest';
 import PostLoginViewController from './PostLoginViewController';
+import Update from '../Icons/Update';
+import { toast } from 'sonner';
 
 const PostLoginView = () => {
 
@@ -13,110 +15,103 @@ const PostLoginView = () => {
     trigerupload,
     trigersubmit,
     handleUpload,
-    FormData,
+    NewUser,
+    uploadRef,
+    submitRef,
     errors,
-    UserExist
+    haserrors,
   } = PostLoginViewController();
 
+  useEffect(() => {
+    console.log(haserrors && errors.length > 0)
+    if (haserrors && errors.length > 0) {
+      toast.error(errors);
+    }
+  }, [haserrors]);
+
   return (
-    <div className="grid grid-cols-2 h-[100vh] w-[100wh]">
-      <div id="login_section" className="w-full h-full overflow-hidden">
-        <div id="logo" className="flex justify-center mt-[7.59vh]">
-          <img src={logo} alt="logo" className=" w-[13.58vw]" />
+    <div className="grid sm:grid-cols-2 h-screen w-full grid-cols-1">
+      <div id="login_section" className="w-full h-full overflow-hidden bg-background">
+        <div id="logo" className="flex justify-center mt-5 sm:mt-7 md:mt-10 lg:mt-14 xl:mt-17">
+          <img src={logo} alt="logo" className="w-32 sm:w-36 md:w-40 lg:w-44 xl:w-48" />
         </div>
         <div
           id="providers_section"
-          className="flex flex-col mt-[18.14vh] items-center w-full h-full"
+          className="flex flex-col mt-20 sm:mt-24 md:mt-28 lg:mt-32 xl:mt-40 items-center w-full h-full"
         >
           <div
             id="file1"
-            className="relative rounded-full h-[14.16vh] w-[14.16vh] cursor-pointer"
+            className="relative rounded-full h-20 sm:h-24 md:h-28 lg:h-32 xl:h-36 w-20 sm:w-24 md:w-28 lg:w-32 xl:w-36 cursor-pointer"
             onClick={trigerupload}
           >
             <img
-              src={FormData.avatar ? FormData.avatar : ""}
+              src={NewUser?.profile.avatar || ""}
               alt="img"
               id="file1"
-              className="absolute rounded-full h-[14.16vh] w-[14.16vh] "
+              className="absolute rounded-full h-20 sm:h-24 md:h-28 lg:h-32 xl:h-36 w-20 sm:w-24 md:w-28 lg:w-32 xl:w-36"
             />
-            <div className="flex justify-end h-full w-full z-15 mt-[6px] ">
-              <img
-                id="file1"
-                src={update}
-                alt=""
-                className="absolute h-[3.33vh] w-[3.33vh] z-15"
-              />
+            
+            <div className="flex justify-end h-full w-full " >
+              <Update
+                className="z-10 mt-1 sm:mt-1.5 mt-1 sm:mt-1.5 md:mt-2 lg:mt-2.5 xl:mt-3 "
+                width="24"
+                height="24"
+              />  
             </div>
           </div>
           <input
+            ref={uploadRef}
             type="file"
             id="file1"
             name="file1"
             className="hidden"
             onChange={handleUpload}
           />
-          <div id="inputs" className="flex mt-[3.42vh]">
+          <div id="inputs" className="flex mt-6 sm:mt-7 md:mt-8 lg:mt-9 xl:mt-10">
             <form onSubmit={handlesubmit}>
               <div className="flex flex-col">
-                  <p className={` ${errors.name?"block":"hidden"} ml-1 text-[1.48vh]  text-[orange]`}>PLEASE ENTER A USER NAME</p>
-                  <p className={` ${UserExist?"block":"hidden"} ml-1 text-[1.48vh]  text-[orange]`}>USER NAME ALREADY EXIST</p>
-                <div className=" h-[5.92vh] w-[14.57vw] rounded-[1.29vh] bg-[#1E1F23] border-[1px]  text-[1.48vh] border-[#3E4048]">
-                  <input
-                    className=" ml-[1.14vw] h-full w-[90%] bg-[#1E1F23] mb-2 text-[1.48vh] focus:outline-none"
-                    name="name"
+                  <input 
+                    className="input-placeholder h-12 sm:h-13 md:h-14 w-60 sm:w-64 md:w-72 rounded-lg sm:rounded-xl md:rounded-2xl bg-input-color border border-input-border-color font-inter text-input-text-color text-sm sm:text-base focus:outline-none" 
+                    name="username"
+                    style={{ paddingLeft: '1rem' }}
                     type="text"
-                    value={FormData.name?FormData.name:""}
-                    onChange={handleInput}
+                    value={NewUser?.username || ""}
                     placeholder="User Name"
-                  />
-                </div>
-                <div className="mt-[1.29vh] h-[5.92vh] w-[14.57vw] rounded-[1.29vh] bg-[#1E1F23] border-[1px] text-[1.48vh] border-[#3E4048]">
-                  <input
-                    className="ml-[1.14vw] h-full w-[90%] bg-[#1E1F23] rounded-[14px] focus:outline-none text-[1.48vh]"
-                    name="displayname"
-                    type="text"
-                    value={FormData.displayname?FormData.displayname:""}
                     onChange={handleInput}
-                    placeholder="Display Name"
+                    autoComplete="off"
                   />
-                  <p className={`${errors.displayname?"block":"hidden"} text-[orange] mt-1`}>PLEASE ENTER A DISPLAY NAME</p>
-                </div>
+                <input 
+                    className="input-placeholder mt-3 sm:mt-4 md:mt-5 h-12 sm:h-13 md:h-14 w-60 sm:w-64 md:w-72 rounded-lg sm:rounded-xl md:rounded-2xl bg-input-color border border-input-border-color text-input-text-color font-inter text-sm sm:text-base focus:outline-none" 
+                    name="display_name"
+                    style={{ paddingLeft: '1rem' }}
+                    type="text"
+                    value={NewUser?.display_name || ""}
+                    placeholder="Display Name"
+                    onChange={handleInput}
+                    autoComplete="off"
+                  />
                 <div className="w-full flex justify-center mt-[3.24vh]">
-                  <div
-                    className="w-[8.12vw] h-[6.29vh] cursor-pointer"
+                  <button
+                    className="w-40 h-16 bg-vibrantOrange text-white text-center font-bold text-xl cursor-pointer"
                     onClick={trigersubmit}
                   >
-                    <CornerLinedCardTest
-                      childComp={
-                        <h2 className={`play font-Rowdies text-[2.59vh]`}>
-                          {' '}
-                          SAVE{' '}
-                        </h2>
-                      }
-                      fill="[color:#FE5821]"
-                      cornerredius="2"
-                      stroke="[color:#FE5821]"
-                      cornershape={[24, 0, 24, 0]}
-                      strokesize={0}
-                      width={200}
-                      height={87}
-                      margine=""
-                      ratio={43 / 100}
-                    />
-                  </div>
+                    SAVE
+                  </button>
                   <input
+                    ref={submitRef}
                     type="submit"
                     value="submit"
                     name="submit"
                     className="hidden"
                   />
-                </div>
+
+                </div>            
               </div>
             </form>
           </div>
         </div>
       </div>
-      <div id="loginIlustration" className="w-full h-full bg-[#FE5821]"></div>
+      <div id="loginIlustration" className="hidden sm:block w-full h-full bg-vibrantOrange"></div>
     </div>
   );
 };
