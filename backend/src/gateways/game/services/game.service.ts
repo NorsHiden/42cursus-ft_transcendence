2a890efead5a113d5086c88f96842c9ac576dd06
nelidris <nelidris@student.1337.ma>
2023-11-18 18:29:07 +0100

    implementing game_mode matchmaking

--- backend/src/gateways/game/services/game.service.ts
+++ backend/src/gateways/game/services/game.service.ts
@@ -139,4 +139,96 @@ export class GameService {
       game_id: game_id,
     };
   }
+
+  // Find a lobby for a client based on game mode
+  findLobby(client: Socket, server: Server, game_mode: string) {
+    // Check if the client is already in a game
+    if (
+      this.ingame.find(
+        (game) =>
+          game.home_player.id == client.id || game.away_player.id == client.id,
+      )
+    )
+      return {
+        action: 'ALREADY_INGAME',
+        message: 'You are already in a game.',
+      };
+
+    // Find an opponent in the lobby with the same game mode
+    const opponent = this.lobby.find(
+      (opponent) =>
+        game_mode === opponent.game_mode && client.id !== opponent.id,
+    );
+
+    // Prepare client lobby information
+    const clientLobby = {
+      id: client.id,
+      socket: client,
+      game_mode: game_mode,
+      score_points: 0,
+    };
+
+    // If no opponent found, add the client to the lobby
+    if (!opponent) {
+      this.lobby.push(clientLobby);
+      return {
+        action: 'LOBBY',
+        message: 'Looking for an opponent...',
+      };
+    }
+
+    // If opponent found, create a new game and remove both players from the lobby
+    const createdGame: InGame = {
+      id: `${opponent.id}${client.id}`,
+      home_player: opponent,
+      away_player: clientLobby,
+      spectators: [],
+      game_mode: 'N/A',
+      created_at: new Date(),
+      end_at: undefined,
+    };
+    this.ingame.push(createdGame);
+    client.join(createdGame.id);
+    opponent.socket.join(createdGame.id);
+
+    // Notify the opponent about the match found
+    const matchFound = {
+      action: 'MATCH_FOUND',
+      message: 'Match Found',
+      game_id: createdGame.id,
+    };
+    server.to(opponent.socket.id).emit('lobby', matchFound);
+
+    // Remove players from the lobby
+    this.lobby = this.lobby.filter((user) => user.id != opponent.id);
+
+    // Return match details
+    return matchFound;
+  }
+
+  // Allow a client to spectate an existing game
+  spectateGame(client: Socket, server: Server, game_id: string) {
+    // Find the index of the game in the 'ingame' array
+    const gameIndex = this.ingame.findIndex((game) => game.id == game_id);
+
+    // If the game is not found, return an error response
+    if (gameIndex < 0)
+      return {
+        action: 'NOT_FOUND',
+        message: 'Game Not Found.',
+      };
+
+    // Add the client as a spectator to the game and join the client to the game room
+    this.ingame[gameIndex].spectators.push({
+      id: client.id,
+    });
+    client.join(game_id);
+
+    // Return a response indicating successful spectating
+    return {
+      action: 'SPECTATE',
+      message: 'Match Found',
+      game_id: game_id,
+    };
+  }
 }
