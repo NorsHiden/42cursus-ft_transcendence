8ebad9c32cba8b4932e99a1a341d434bc1ef8e1f
nelidris <nelidris@student.1337.ma>
2023-11-18 18:29:07 +0100

    implementing game_mode matchmaking

--- backend/src/gateways/game/gateway/game.gateway.ts
+++ backend/src/gateways/game/gateway/game.gateway.ts
@@ -26,15 +26,49 @@ export class GameGateway {
     @Inject(Services.Game) private readonly gameService: IGameService,
   ) {}
 
-  @SubscribeMessage('test')
-  async test(
-    @ConnectedSocket() client: Socket,
-    @MessageBody('name') name: string,
+  // Event handler for when a client connects to the WebSocket server
+  async handleConnection(client: Socket) {
+    // Call the game service to handle the connection
+    await this.gameService.handleConnection(client);
+  }
+
+  // Event handler for when a client disconnects from the WebSocket server
+  async handleDisconnect(client: Socket) {
+    // Call the game service to close the connection
+    await this.gameService.closeConnection(client);
+  }
+
+  // Event handler for the 'lobby' message, used for joining game lobbies
+  @SubscribeMessage('lobby')
+  async lobby(
+    @ConnectedSocket() client: Socket, // Decorator to inject the connected socket
+    @MessageBody('game_mode') game_mode: string, // Decorator to extract the 'game_mode' from the message body
   ) {
-    await this.gameService.handleConnection(client, client.id);
-    console.log(this.gameService.getId(client.id));
-    client.join('some_room');
-    this.server.to('some_room').emit('test2', 'test2');
-    return name;
+    // Check if the provided game mode is valid
+    if (
+      game_mode !== 'REGULAR' &&
+      game_mode !== 'CURSED' &&
+      game_mode !== 'VANISH' &&
+      game_mode !== 'GOLD_RUSH'
+    ) {
+      // If not valid, return an error response
+      return {
+        action: 'NOT_FOUND',
+        message: 'Game Mode Not Found',
+      };
+    }
+
+    // If valid, call the game service to find a lobby
+    return this.gameService.findLobby(client, this.server, game_mode);
+  }
+
+  // Event handler for the 'spectate' message, used for spectating games
+  @SubscribeMessage('spectate')
+  async spectate(
+    @ConnectedSocket() client: Socket, // Decorator to inject the connected socket
+    @MessageBody('game_id') game_id: string, // Decorator to extract the 'game_id' from the message body
+  ) {
+    // Call the game service to handle the spectate request
+    return this.gameService.spectateGame(client, this.server, game_id);
   }
 }
