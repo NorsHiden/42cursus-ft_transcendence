301bf40abb3a6baa3ed00010ed401df042b71c55
nelidris <nelidris@student.1337.ma>
2023-11-21 13:53:21 +0100

    rebase on backend

--- backend/src/multer/multer.service.ts
+++ backend/src/multer/multer.service.ts
@@ -14,9 +14,13 @@ export class MulterConfigService implements MulterOptionsFactory {
         destination: (req: any, file, cb) => {
           // Creates a directory for storing user avatars if it doesn't exist.
           let dir = '../imgs';
-          if (file.fieldname == 'avatar' || file.fieldname == 'avatar')
-            dir += `/users/${file.fieldname}s/${req.user.sub}`;
-          else dir += `/channels/${file.fieldname}`;
+
+          if (req.path.includes('users')) {
+            dir += `/users/${file.fieldname}s/${req.user.id}`;
+          } else if (req.path.includes('channels')) {
+            dir += `/channels/${file.fieldname}s/${req.user.id}`;
+          }
+
           if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
           }
