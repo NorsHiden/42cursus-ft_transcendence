fe14b05d156d825550a072e335f6514b3d6bbfa8
Ereur <amoussaouianas@gmail.com>
2023-11-22 12:54:40 +0100

    adding some golobale enums

---
+++ frontend/src/globalTypes/types.tsx
@@ -0,0 +1,26 @@
+export type UserProfile = {
+    id: number;
+    about: string;
+    avatar: string;
+    banner: string;
+  }
+  
+export type User = {
+    id: number;
+    username: string;
+    display_name: string;
+    email: string;
+    wins: number;
+    loses: number;
+    points: number;
+    verified: boolean;
+    profile: UserProfile;
+}
+
+export enum ScreenType {
+  BigScreen = 'bigScreen',
+  DesktopOrLaptop = 'desktopOrLaptop',
+  TabletOrMobile = 'tabletOrMobile',
+  Portrait = 'portrait',
+  Unknown = 'unknown'
+}
