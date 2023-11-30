2da06919e85a2c0502c0a8b537514e4b089b2e3e
Ereur <amoussaouianas@gmail.com>
2023-11-30 01:11:53 +0100

    clean up types

--- frontend/src/globalTypes/index.tsx
+++ frontend/src/globalTypes/index.tsx
@@ -1,1 +1,1 @@
-import User from './types';
+export * from './types';
--- frontend/src/globalTypes/types.tsx
+++ frontend/src/globalTypes/types.tsx
@@ -1,4 +1,4 @@
-type UserProfile = {
+export type UserProfile = {
     id: number;
     about: string;
     avatar: string;
@@ -7,7 +7,7 @@ type UserProfile = {
     birthdate: string;
   }
   
-type User = {
+export type User = {
     id: number;
     username: string;
     display_name: string;
@@ -17,6 +17,29 @@ type User = {
     points: number;
     verified: boolean;
     profile: UserProfile;
+    isforeign: boolean;
+    isfriend: boolean;
 }
 
-export default User
+
+
+export enum Game {
+  CURSED = "cursed",
+  GOLDRUSH = "goldrush",
+  VANISH = "vanish",
+  REGULAR = "regular"
+
+}
+
+export enum CardType {
+  RECENT_MATCHES = "recent_matches",
+  MATCH_HISTORY = "match_history" 
+}
+
+
+export type player = {
+  name: string;
+  score: number;
+  avatar: string;
+}
+// export default User
