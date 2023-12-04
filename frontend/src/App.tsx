39a173f7310e9cf1dd5a43e7c9768337ef6e77d2
Peannut <zakariaspam0@gmail.com>
2023-12-04 18:31:57 +0100

    2fa-verification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,10 @@ import Home from '@pages/Home';
 import Login from '@pages/Login';
 import { Discovery } from '@pages/Discovery';
 
-import PostLogin,{ postLoginLoader } from '@pages/PostLogin';
-import Profile,{profileLoader} from '@pages/Profile';
-import {Overview,MatchHistory,Achievements,Settings,ManageFriends} from '@components/profile';
+import PostLogin, { postLoginLoader } from '@pages/PostLogin';
+import Profile, { profileLoader } from '@pages/Profile';
+import { Overview, MatchHistory, Achievements, Settings, ManageFriends } from '@components/profile';
+import OTP2fa, { otpLoader } from '@pages/OTP2fa';
 
 function Layoutloader() {
   return {
@@ -24,14 +25,11 @@ function Layoutloader() {
 
 // import {Settings} from '@components/profile';
 
-
-
-
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    loader:Layoutloader,
+    loader: Layoutloader,
     children: [
       {
         path: '/home',
@@ -42,33 +40,32 @@ const router = createBrowserRouter([
         element: <Discovery />,
       },
       {
-        path:'/:user',
-        id:'profile',
-        element: <Profile/>,
-        loader:({ params }) =>
-          profileLoader(params.user),
-        children:[
+        path: '/:user',
+        id: 'profile',
+        element: <Profile />,
+        loader: ({ params }) => profileLoader(params.user),
+        children: [
           {
-            path:'/:user/settings',
-            element:<Settings/>
+            path: '/:user/settings',
+            element: <Settings />,
           },
           {
-            path:'/:user/overview',
-            element:<Overview/>
+            path: '/:user/overview',
+            element: <Overview />,
           },
           {
-            path:'/:user/matchhistory',
-            element:<MatchHistory/>
+            path: '/:user/matchhistory',
+            element: <MatchHistory />,
           },
           {
-            path:'/:user/achievements',
-            element:<Achievements/>
+            path: '/:user/achievements',
+            element: <Achievements />,
           },
           {
-            path:'/:user/friends',
-            element:<ManageFriends/>
+            path: '/:user/friends',
+            element: <ManageFriends />,
           },
-        ]
+        ],
       },
     ],
   },
@@ -80,6 +77,11 @@ const router = createBrowserRouter([
     path: '/postlogin',
     loader: postLoginLoader,
     element: <PostLogin />,
+  },
+  {
+    path: '/2fa-verification',
+    loader: otpLoader,
+    element: <OTP2fa />,
   },
 ]);
 
