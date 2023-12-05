7dfb3a418c1261d5759a03eca122f00f448609ab
SsiCipher <SsiCipher@github.com>
2023-12-05 10:37:43 +0100

    🟩 ADD: added achievement component + finished achievements tab in user profile

--- frontend/src/components/profile/Achievements/AchievementsView.tsx
+++ frontend/src/components/profile/Achievements/AchievementsView.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import twclsx from '@utils/twclsx';
+import React, { useEffect, useRef, useState } from 'react';
 import LockSolid from '@assets/novaIcons/solid/LockSolid';
-import { AchievementType, achievements } from '@globalTypes/achievements';
+import { AchievementType, achievements, allAchievements } from '@globalTypes/achievements';
+import { getColorValue } from '@utils/getColorValue';
 
 type AchievementProps = {
   isClaimed: boolean;
@@ -12,23 +12,28 @@ type AchievementProps = {
 
 const Achievement: React.FC<AchievementProps> = ({ isClaimed, type, title, description }) => {
   const Icon = achievements[type].icon;
-  const [isTooltipShown, setIsTooltipShown] = useState(true);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    setTooltipPosition({ x: event.clientX + 30, y: event.clientY + 30 });
+  };
 
   return (
     <div
-      className="relative center p-4 rounded-xl cursor-pointer"
-      style={{ backgroundColor: achievements[type].color }}
+      onMouseMove={handleMouseMove}
+      className="group relative center p-4 rounded-lg cursor-pointer"
+      style={{
+        backgroundColor: isClaimed ? achievements[type].color : getColorValue('lightBlack'),
+      }}
     >
-      {!isClaimed && <LockSolid />}
-      <Icon size={36} className="text-black" />
+      {!isClaimed && <LockSolid size={32} className="absolute text-white" />}
+      <Icon size={40} className={isClaimed ? 'text-black' : 'text-gray'} />
       <div
-        className={twclsx(
-          'absolute bg-gray top-full left-full w-[max-content]',
-          isTooltipShown ? 'block' : 'hidden',
-        )}
+        className="fixed top-0 left-0 z-50 w-[max-content] hidden group-hover:block bg-lightBlack py-4 px-6 rounded-lg"
+        style={{ transform: `translate(${tooltipPosition.x}px, ${tooltipPosition.y}px)` }}
       >
-        <h1 className="text-black">{title}</h1>
-        <h2 className="text-black">{description}</h2>
+        <h1 className="text-white text-xl/10 font-serif font-light">{title}</h1>
+        <h2 className="text-white/70">{description}</h2>
       </div>
     </div>
   );
@@ -36,13 +41,23 @@ const Achievement: React.FC<AchievementProps> = ({ isClaimed, type, title, descr
 
 const AchievementsView: React.FC = () => {
   return (
-    <div id="Achievement" className="py-24 flex text-white">
-      <Achievement
-        isClaimed={true}
-        type={AchievementType.WELCOME_ABOARD}
-        title="Welcome Aboard!"
-        description="Logging in for the very first time."
-      />
+    <div id="Achievement" className="max-w-screen-lg py-24 flex flex-wrap gap-10 text-white">
+      {allAchievements.map((achievement) => (
+        <Achievement
+          isClaimed={true}
+          type={achievement[0]}
+          title={achievement[1]}
+          description="This will be a small description"
+        />
+      ))}
+      {allAchievements.map((achievement) => (
+        <Achievement
+          isClaimed={false}
+          type={achievement[0]}
+          title={achievement[1]}
+          description="This will be a small description"
+        />
+      ))}
     </div>
   );
 };
